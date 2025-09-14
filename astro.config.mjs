de79e3cb4448c5c17550e59d38532954a297e3d9
Enrique Morcillo Martínez <kikemorcillo03@gmail.com>
2025-09-14 12:43:05 +0200

    cambios en la configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-// import staticAdapter from '@astrojs/adapter-static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // adapter: staticAdapter(),
   base: '/portfolioWeb/' // Cambia esto si tu repo tiene otro nombre
 });
